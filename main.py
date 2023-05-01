be892f6dfcf9dc8b10c572809d7d179becefdd5c
benrachmut <rachmut.ben@gmail.com>
2023-05-01 15:53:06 +0300

    start simple example for arseni

--- main.py
+++ main.py
@@ -779,12 +779,15 @@ if __name__ == '__main__':
     
     a1 = AgentSimulation("1",Location(0,0),domain=["a","b"])
     a2 = AgentSimulation("2",Location(10,10),domain=["a","b"])
+    a3 = AgentSimulation("3",Location(5,5),domain=["a","b"])
 
-    a1.neighbours_ids_list=["2"]
-    a2.neighbours_ids_list=["1"]
+    a1.neighbours_ids_list=["2","3"]
+    a2.neighbours_ids_list=["1","3"]
+    a3.neighbours_ids_list=["2","3"]
 
     aa1 = AgentArseni(a1,value="a")
     aa2 = AgentArseni(a2,value="b")
+    aa3 = AgentArseni(a3,value="a")
 
 
 
